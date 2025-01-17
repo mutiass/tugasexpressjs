fd7f003f9873d1020da4bededadd21eeaefde2c0
mutiass <158068300+mutiass@users.noreply.github.com>
2025-01-17 19:44:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/portosqlmutia', express.static(path.join(__dirname, 'portosqlmutia')));
-
 app.use('/portosqlmutia2', express.static(path.join(__dirname, 'portosqlmutia2')));
 
-app.listen(PORT, () => {
-    console.log(`Server Berjalan di: http://localhost:${PORT}`);
+app.get('/portosqlmutia', (req, res) => {
+    res.sendFile(path.join(__dirname, 'portosqlmutia', 'index.html'));
 });
+
+app.get('/portosqlmutia2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'portosqlmutia2', 'index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Ekspor app untuk digunakan oleh Vercel
+module.exports = app;
