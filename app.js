38e12a35d152a212dc55b7f8473dd9cff945897c
Mutia Sakinah <wahababdulhadimohammed@gmail.com>
2025-01-17 16:03:59 +0800

    my commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/portosqlmutia', express.static(path.join(__dirname, 'portosqlmutia')));
+
+app.use('/portosqlmutia2', express.static(path.join(__dirname, 'portosqlmutia2')));
+
+app.listen(PORT, () => {
+    console.log(`Server Berjalan di: http://localhost:${PORT}`);
+});
